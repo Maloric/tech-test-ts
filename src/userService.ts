b68efb4db56598b2960e91220e28ca8b50788b73
Jim Speakman <jim@jimspeakman.net>
2021-04-06 22:55:42 +0100

    Jim's Observable mashup

--- src/userService.ts
+++ src/userService.ts
@@ -1,10 +1,24 @@
 import { DataService } from './dataService';
 import { of, Observable } from 'rxjs';
+import {tap, map, mergeMap} from "rxjs/operators";
 
 export class UserService {
     constructor(private dataService: DataService) {}
 
     getUsersWithPopularPosts(commentThreshold: number): Observable<any[]> {
-        return of([]);
+        return this.dataService.getComments().pipe(
+            map ( comments => comments.reduce((commentCount, comment) => {
+                commentCount[comment.postId] = (commentCount[comment.postId] || 0) + 1;
+                return commentCount;
+            }, {} )),
+            mergeMap(commentCount => this.dataService.getPosts().pipe(
+                map(posts => posts
+                    .filter(post => commentCount[post.id.toString()] >= commentThreshold)
+                    .map(post => post.userId))
+            )),
+            mergeMap(popularUserIds => this.dataService.getUsers().pipe(
+                map(users => users.filter(user => popularUserIds.find( userId => userId === user.id)))
+            ))
+        );
     }
 }
